9bb8940f84f6a1c691866da4aec8a0ec5a92733f
gergo <info@gazdagergo.com>
2017-11-20 06:00:51 +0100

    user id not found fix

--- src/pages/user/page.js
+++ src/pages/user/page.js
@@ -3,15 +3,40 @@ import userApi from "../../services/userApi";
 import { Link } from 'react-router';
 
 export default class UserPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null,
+      userNotExists: false
+    }
+  }
+  
   componentDidMount(){
     userApi.getUsers(this.props.params.id)
     .then(results => (results.json()))
     .then(data => {
-      this.setState({ user: data })
+      if (!data) {
+        this.setState({ userNotExists: true })
+      } else {
+        this.setState({ user: data })
+      }
     })
+    .catch(console.error)
   }
 
   render() {
+    if (this.state.userNotExists) {
+      return (
+        <div className="container user-container">
+          <header>
+            <div className="back"><Link to={`/`}>Back to users</Link></div>
+          </header>
+          <main>
+            <div>User with id {this.props.params.id} not found</div>
+          </main>
+        </div>
+      )
+    }
     if (!this.state || !this.state.user) {
       return null;
     }
